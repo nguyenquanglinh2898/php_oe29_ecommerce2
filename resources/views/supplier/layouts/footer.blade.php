f8c04bc492fff25d575fde1aa295d3cab79c73fd
tran khac nam <nam3061998@gmail.com>
2020-10-04 15:21:10 +0700

    layout supplier

---
+++ resources/views/supplier/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>{{ trans('adminlte') }}</b> {{ trans('version') }}
+    </div>
+    <strong>{{ trans('copyright') }} &copy; {{ config('config.number') }} <a href="#">{{ config('config.name') }}</a>.</strong> {{ trans('supplier.all_rights_reserved') }}
+</footer>
